756a8bb91bb34066545ec3c01a3ada86a76adfb1
İlteriş Eroğlu <ilteris@asenkron.com.tr>
2021-04-26 19:18:25 +0000

    eslint pass and non-intrusive structural change

--- sun.js
+++ sun.js
@@ -1,44 +1,72 @@
 // Nodejs encryption with CTR
-const crypto = require('crypto')
-const aesCmac = require('node-aes-cmac').aesCmac
+const crypto = require("crypto");
+const aesCmac = require("node-aes-cmac").aesCmac;
 
-function calculateCmacData(uid, readCnt, key) {
+/**
+ * @param {string} uid
+ * @param {number} readCnt
+ * @param {Buffer} key
+ * @returns {Buffer}
+ */
+const calculateCmacData = (uid, readCnt, key) => {
     let piccData = Buffer.alloc(10);
-    Buffer.from(uid, 'hex').copy(piccData);
+    Buffer.from(uid, "hex").copy(piccData);
     piccData.writeIntLE(readCnt, 7, 3);
-    return calculateCmacBuffer(piccData, key)
-}
 
-function calculateCmacBuffer(piccData, key) {
+    return calculateCmacBuffer(piccData, key);
+};
+
+/**
+ * @param {Buffer} piccData
+ * @param {Buffer} key
+ * @returns {Buffer}
+ */
+const calculateCmacBuffer = (piccData, key) => {
     let sv2Data = Buffer.alloc(16);
-    sv2Data.write('3CC300010080', 'hex') // SV2 as per NXP DS465430 (NT4H2421Gx Data sheet)
+    sv2Data.write("3CC300010080", "hex"); // SV2 as per NXP DS465430 (NT4H2421Gx Data sheet)
+
     piccData.copy(sv2Data, 6);
+
     let sv2 = aesCmac(key, sv2Data, {returnAsBuffer: true});
     let fullCmac = aesCmac(sv2, Buffer.alloc(0), {returnAsBuffer: true});
     let cmac = Buffer.alloc(8);
+
     for (let i = 0; i < 8; i++) {
         fullCmac.copy(cmac, i, i*2+1, i*2+2);
     }
-    return cmac
-}
 
-function decryptPicc(PICCENCData, key) {
-    let decipher = crypto.createDecipheriv('aes-128-cbc', key, Buffer.alloc(16))
-    decipher.setAutoPadding(false)
-    let decrypted = decipher.update(PICCENCData)
-    tagPicc = Buffer.concat([decrypted, decipher.final()])
+    return cmac;
+};
 
+/**
+ * @typedef {Object} decryptedPicc
+ * @property {string} PICC_DataTag
+ * @property {string} uid
+ * @property {string} cnt
+ * @property {number} cntInt
+ */
+
+/**
+ * @param {Buffer} PICCENCData 
+ * @param {Buffer} key
+ * @returns {decryptedPicc}
+ */
+const decryptPicc = (PICCENCData, key) => {
+    let decipher = crypto.createDecipheriv("aes-128-cbc", key, Buffer.alloc(16));
+    decipher.setAutoPadding(false);
+    let decrypted = decipher.update(PICCENCData);
+    let tagPicc = Buffer.concat([decrypted, decipher.final()]);
 
     return {
         PICC_DataTag: tagPicc.slice(0, 1).toString("hex"),
         uid: tagPicc.slice(1, 8).toString("hex"),
         cnt: tagPicc.slice(8, 11).toString("hex"),
         cntInt: tagPicc.readUIntLE(8, 3)
-    }
-}
+    };
+};
 
 module.exports = {
     calculateCmacData,
     calculateCmacBuffer,
     decryptPicc
-}
+};
--- test/verify.js
+++ test/verify.js
@@ -1,7 +1,9 @@
-const sun  = require("../sun")
+const sun  = require("../sun");
 
-const key = Buffer.from("00000000000000000000000000000000", 'hex')
-let encryptedText = Buffer.from("EF963FF7828658A599F3041510671E88", 'hex')
-let data = sun.decryptPicc(encryptedText, key);
-console.log(data)
-console.log(sun.calculateCmacData(data.uid, data.cntInt, key))
+const key = Buffer.from("00000000000000000000000000000000", "hex");
+const encryptedText = Buffer.from("EF963FF7828658A599F3041510671E88", "hex");
+
+const data = sun.decryptPicc(encryptedText, key);
+
+console.log(data);
+console.log(sun.calculateCmacData(data.uid, data.cntInt, key));
