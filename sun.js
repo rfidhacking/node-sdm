3178282be1747e6321b2f1cf42b65e3f7c48cdae
Anže Jenšterle <anze@anze.xyz>
2021-04-26 20:13:21 +0200

    Added CMAC calculation

--- sun.js
+++ sun.js
@@ -1,7 +1,26 @@
 // Nodejs encryption with CTR
 const crypto = require('crypto')
+const aesCmac = require('node-aes-cmac').aesCmac
 
+function calculateCmacData(uid, readCnt, key) {
+    let piccData = Buffer.alloc(10);
+    Buffer.from(uid, 'hex').copy(piccData);
+    piccData.writeIntLE(readCnt, 7, 3);
+    return calculateCmacBuffer(piccData, key)
+}
 
+function calculateCmacBuffer(piccData, key) {
+    let sv2Data = Buffer.alloc(16);
+    sv2Data.write('3CC300010080', 'hex') // SV2 as per NXP DS465430 (NT4H2421Gx Data sheet)
+    piccData.copy(sv2Data, 6);
+    let sv2 = aesCmac(key, sv2Data, {returnAsBuffer: true});
+    let fullCmac = aesCmac(sv2, Buffer.alloc(0), {returnAsBuffer: true});
+    let cmac = Buffer.alloc(8);
+    for (let i = 0; i < 8; i++) {
+        fullCmac.copy(cmac, i, i*2+1, i*2+2);
+    }
+    return cmac
+}
 
 function decryptPicc(PICCENCData, key) {
     let decipher = crypto.createDecipheriv('aes-128-cbc', key, Buffer.alloc(16))
@@ -18,4 +37,8 @@ function decryptPicc(PICCENCData, key) {
     }
 }
 
-module.exports.decryptPicc=decryptPicc
+module.exports = {
+    calculateCmacData,
+    calculateCmacBuffer,
+    decryptPicc
+}
--- test/verify.js
+++ test/verify.js
@@ -1,6 +1,7 @@
 const sun  = require("../sun")
 
 const key = Buffer.from("00000000000000000000000000000000", 'hex')
-let encryptedText = Buffer.from("4875ED0DA384F16DC932D021920E930B", 'hex')
-console.log(sun.decryptPicc(encryptedText, key))
-
+let encryptedText = Buffer.from("EF963FF7828658A599F3041510671E88", 'hex')
+let data = sun.decryptPicc(encryptedText, key);
+console.log(data)
+console.log(sun.calculateCmacData(data.uid, data.cntInt, key))
